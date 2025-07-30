750b3ecc40430660741f0d7330db62ddd5ac5bb2
Parker Corbitt <parkercorbitt36@gmail.com>
2025-07-29 21:20:55 -0400

    Changes for cross compatibility

--- musviz.cc
+++ musviz.cc
@@ -90,7 +90,7 @@ int main(int argc, char* argv[])
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
-        while (const std::optional event = window.pollEvent())
+        while (std::optional event = window.pollEvent())
         {
             // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
